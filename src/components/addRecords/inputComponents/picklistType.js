30967f49385bd4c1c4c0a28d92103a2bdb6472f7
Kishan <kishansutariya.coderkube@gmail.com>
2023-09-20 11:03:02 +0530

    Default value for drop down

--- src/components/addRecords/inputComponents/picklistType.js
+++ src/components/addRecords/inputComponents/picklistType.js
@@ -1,54 +1,64 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {View} from 'react-native';
-import { Picker } from '@react-native-picker/picker';
+import {Picker} from '@react-native-picker/picker';
 import {commonStyles} from '../../../styles/common';
 
 class PickListType extends Component {
-    constructor(props) {
-        super(props);
-        let val = (this.props.obj.lable === 'Company country') ? '' : this.props.obj.type.defaultValue;
-        val = (this.props.obj.currentValue !== undefined) ? this.props.obj.currentValue : val;
-// console.log('--component data--');
-// console.log(this.props.obj.currentValue);
-// console.log(val);
-        this.state = {
-            saveValue: val,
-            fieldName: this.props.obj.name
-        };
-    }
+  constructor(props) {
+    super(props);
+    let val =
+      this.props.obj.lable === 'Company country'
+        ? ''
+        : this.props.obj.type.defaultValue;
+    val =
+      this.props.obj.currentValue !== undefined
+        ? this.props.obj.currentValue
+        : val;
+    // console.log('--component data--');
+    // console.log(this.props.obj.currentValue);
+    // console.log(val);
+    this.state = {
+      saveValue: val,
+      fieldName: this.props.obj.name,
+    };
+  }
 
-    render() {
-        let options = this.props.obj.type.picklistValues;
+  render() {
+    let options = this.props.obj.type.picklistValues;
 
-        return (
-            <View style={commonStyles.inputHolder}>
-                {this.props.fieldLabelView}
-                <View style={{ flex: 1 }}>
-                    <Picker
-                        mode='dropdown'
-                        selectedValue={this.state.saveValue}
-                        onValueChange={(itemValue) => {
-                            //disable save 'Please Select' value
-                            // if (itemValue !== 0) {
-                                this.setState({ saveValue: itemValue });
-                            // }
-                        }}
-                    >
-                        <Picker.Item label='Please Select' value={0} />
-                        {options.map((item, index) => {
-                            return (
-                                <Picker.Item
-                                    label={item.label}
-                                    value={item.value}
-                                    key={index}
-                                />
-                            );
-                        })}
-                    </Picker>
-                </View>
-            </View>
-        );
-    }
+    return (
+      <View style={commonStyles.inputHolder}>
+        {this.props.fieldLabelView}
+        <View style={{flex: 1}}>
+          <Picker
+            mode="dropdown"
+            selectedValue={this.state.saveValue}
+            onValueChange={(itemValue) => {
+              //disable save 'Please Select' value
+              // if (itemValue !== 0) {
+              this.setState({saveValue: itemValue});
+              // }
+            }}>
+            <Picker.Item
+              label={
+                this.state.saveValue ? 'Select an option' : 'Please Select'
+              }
+              value={0}
+            />
+            {options.map((item, index) => {
+              return (
+                <Picker.Item
+                  label={item.label}
+                  value={item.value}
+                  key={index}
+                />
+              );
+            })}
+          </Picker>
+        </View>
+      </View>
+    );
+  }
 }
 
 export default PickListType;
