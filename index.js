47f7242f357973a20915e942599fa26374de6e44
Ali Hamza <alihamzapvt445@gmail.com>
2025-12-04 01:12:20 +0500

    first commit

--- index.js
+++ index.js
@@ -1,67 +1,8 @@
-import React, {Component} from 'react';
-import {Provider} from 'react-redux';
-import {AppRegistry, LogBox, Platform, UIManager} from 'react-native';
-import {name as appName} from './app.json';
-import Router from './src/router';
-import store from './src/store';
-import messaging from '@react-native-firebase/messaging';
-import {onDisplayNotification} from './NotificationService';
+/**
+ * @format
+ */
 
-LogBox.ignoreAllLogs();
-console.disableYellowBox = true;
-
-messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-  onDisplayNotification(
-    remoteMessage?.data?.title,
-    remoteMessage?.data?.body,
-    remoteMessage?.data,
-  );
-});
-
-// notifee.onBackgroundEvent(async ({type, detail}) => {
-//   const {notification, pressAction} = detail;
-
-//   // Check if the user pressed the "Mark as read" action
-//   if (type === EventType.ACTION_PRESS && pressAction.id === 'mark-as-read') {
-//     // Remove the notification
-//     if (notification?.data) {
-//       if (notification?.data?.moduleName) {
-//         if (notification?.data?.recordId) {
-//           navigationRef.navigate('Record Details', {
-//             moduleName: notification?.data?.moduleName,
-//             moduleLable: notification?.data?.moduleName,
-//             recordId: notification?.data?.recordId,
-//             navigation: navigationRef,
-//           });
-//         } else {
-//           navigationRef.navigate('Records', {
-//             moduleName: notification?.data?.moduleName,
-//             moduleLable: notification?.data?.moduleName,
-//             navigation: navigationRef,
-//           });
-//         }
-//       } else {
-//         reset([{name: 'Drawer'}]);
-//       }
-//     }
-//     await notifee.cancelNotification(notification?.id);
-//   }
-// });
-
-if (Platform.OS === 'android') {
-  if (UIManager.setLayoutAnimationEnabledExperimental) {
-    UIManager.setLayoutAnimationEnabledExperimental(true);
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <Router />
-      </Provider>
-    );
-  }
-}
-
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
+import App from './src/App';
 AppRegistry.registerComponent(appName, () => App);
